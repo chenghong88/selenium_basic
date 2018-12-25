0cc2d88133f64aa18c8ddc854e8bf16437c32686
chenghong <2633348450@qq.com>
2018-12-25 23:47:40 +0800

    add uploads_files.py

---
+++ uploads_files.py
@@ -0,0 +1,9 @@
+# 三、上传图片到百度--视频005
+# import time
+# from selenium import webdriver
+# driver = webdriver.Chrome()
+# driver.get('https://www.baidu.com')
+# driver.find_element_by_css_selector('#form > span.bg.s_ipt_wr.quickdelete-wrap > span').click()
+# driver.find_element_by_css_selector('#form > div > div.soutu-state-normal > div.upload-wrap > input').send_keys('C:\\Users\\X280\\Desktop\\python_lesson\\baidu.png')
+# time.sleep(10)
+# driver.quit()
